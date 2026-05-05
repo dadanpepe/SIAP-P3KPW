312a07cbfbdc566547ec7a707c7c9f4f6d6a82e8
dadanpepe <36986977+dadanpepe@users.noreply.github.com>
2026-05-05 08:00:56 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -7,7 +7,7 @@
  * - Jika API_BASE_URL diisi → semua request dikirim ke Google Apps Script
  */
 
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxFgKWLI2C3_wnIE9dotC3sdFsfTizWL2DeN_WnzA_leUhyb_AnSrqm-cpavOwlaM6n/exec'; // Kosongkan untuk mode localStorage, isi dengan URL Web App GAS
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbwFcOpNrV7e_5O6ZKO62mUT47sj0EY1Q3NRyd_g4myl7tLRIW17V-1K-HvmhHrydnU8/exec'; // Kosongkan untuk mode localStorage, isi dengan URL Web App GAS
 
 const api = {
 
